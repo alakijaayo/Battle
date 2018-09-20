e13bf22b49040fa670b4085d56bc329680777874
alakijayo <alakijaayo@gmail.com>
2018-09-20 16:40:33 +0100

    Adding player class

---
+++ lib/player.rb
@@ -0,0 +1,12 @@
+class Player
+  attr_reader :name, :health
+
+  def initialize(name)
+    @name = name
+    @health = 100
+  end
+
+  def hit(points)
+    @health -= points
+  end
+end
---
+++ spec/features/players_spec.rb
@@ -0,0 +1,13 @@
+require 'player'
+
+describe Player do
+  subject(:player) { described_class.new('Ayo') }
+  it "should store the player's name" do
+    expect(player.name).to eq 'Ayo'
+  end
+
+  it 'should remove 10 HP from player 2' do
+    player.hit(10)
+    expect(player.health).to eq 90
+  end
+end

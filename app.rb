038967e17f78fdad451072b11d5c4c1271440881
alakijayo <alakijaayo@gmail.com>
2018-09-20 19:40:52 +0100

    Implementing Hit Points

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require './lib/player'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -10,20 +11,23 @@ class Battle < Sinatra::Base
 
   post '/names' do
     p params
-    session[:player1] = params[:player1]
-    session[:player2]= params[:player2]
+    $player1 = Player.new(params[:player1])
+    $player2 = Player.new(params[:player2])
     redirect '/play'
   end
 
   get '/play' do
-    @player1 = session[:player1]
-    @player2 = session[:player2]
+    @player1 = $player1.name
+    @player2 = $player2.name
     erb :play
   end
 
   get '/attack' do
-    @player1 = session[:player1]
-    @player2 = session[:player2]
+    @player1 = $player1.name
+    @player2 = $player2.name
+    $player2.hit(10)
+    @health1 = $player1.health
+    @health2 = $player2.health
     erb :attack
   end
 end
